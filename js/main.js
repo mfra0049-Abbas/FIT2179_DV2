b2a3402ad819437e96fe5ad64cb93d44f16afbb8
Abbas Fraz <mfra0049@student.monash.edu>
2026-06-01 03:11:44 +0800

    Add FIT2179 DV2 dashboard

---
+++ js/main.js
@@ -0,0 +1,103 @@
+vegaEmbed(
+  "#line_ict_access",
+  "visualisations/line_ict_access.vg.json",
+  {
+    actions: false
+  }
+);
+
+vegaEmbed(
+  "#line_urban_rural_access",
+  "visualisations/line_urban_rural_access.vg.json",
+  {
+    actions: false
+  }
+);
+
+vegaEmbed(
+  "#kpi_access_snapshot",
+  "visualisations/kpi_access_snapshot.vg.json",
+  {
+    actions: false
+  }
+);
+
+vegaEmbed(
+  "#s02_v04_digital_behaviour_trend",
+  "visualisations/s02_v04_digital_behaviour_trend.vg.json",
+  {
+    actions: false
+  }
+);
+
+vegaEmbed(
+  "#s02_v05_average_interest_ranking",
+  "visualisations/s02_v05_average_interest_ranking.vg.json",
+  {
+    actions: false
+  }
+);
+
+vegaEmbed(
+  "#s02_v06_average_interest_treemap",
+  "visualisations/s02_v06_average_interest_treemap.vg.json",
+  {
+    actions: false
+  }
+);
+
+vegaEmbed(
+  "#s03_v07_digital_behaviour_score_map",
+  "visualisations/s03_v07_digital_behaviour_score_map.vg.json",
+  {
+    actions: false
+  }
+);
+
+vegaEmbed(
+  "#s03_v08_digital_behaviour_gravity_map",
+  "visualisations/s03_v08_digital_behaviour_gravity_map.vg.json",
+  {
+    actions: false
+  }
+);
+
+vegaEmbed(
+  "#s04_v09_state_score_ranking",
+  "visualisations/s04_v09_state_score_ranking.vg.json",
+  {
+    actions: false
+  }
+);
+
+vegaEmbed(
+  "#s04_v10_region_boxplot",
+  "visualisations/s04_v10_region_boxplot.vg.json",
+  {
+    actions: false
+  }
+);
+
+vegaEmbed(
+  "#s04_v11_dominant_behaviour_by_state",
+  "visualisations/s04_v11_dominant_behaviour_by_state.vg.json",
+  {
+    actions: false
+  }
+);
+
+vegaEmbed(
+  "#s05_v12_population_behaviour_scatter",
+  "visualisations/s05_v12_population_behaviour_scatter.vg.json",
+  {
+    actions: false
+  }
+);
+
+vegaEmbed(
+  "#s05_v13_behaviour_trend_small_multiples",
+  "visualisations/s05_v13_behaviour_trend_small_multiples.vg.json",
+  {
+    actions: false
+  }
+);
